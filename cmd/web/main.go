ca57a7bc9bc7ce5190b8f37304671b034d0d59bb
kolaadebayo <kolabayo360@gmail.com>
2023-04-27 18:00:49 -0600

    define levelled logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 
 func main() {
-	mux := http.NewServeMux()
+	// command line flags
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-	fileServer := http.FileServer(http.Dir("./fileserver/"))
-	
-	mux.Handle("/fileserver/", http.StripPrefix("/fileserver", fileServer))
+	// levelled logs
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/password/create", passwordCreate)
 	mux.HandleFunc("/password/view", passwordView)
 
-	log.Print("Listening on port :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Hardpass is starting on port %s 🚀", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
 }
